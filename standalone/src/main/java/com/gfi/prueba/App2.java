5db6e1ee66513c3e8a15b019f7eda1733a5f2ea8
GorkaLander <gorkalander21@gmail.com>
2020-07-28 08:31:27 +0000

    mi app maven 1

---
+++ standalone/src/main/java/com/gfi/prueba/App2.java
@@ -0,0 +1,13 @@
+package com.gfi.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
